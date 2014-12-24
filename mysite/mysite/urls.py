27db083bc81016734f1e1e0d029e21d7b01ca36b
GotThatHub <sperry@lrwonline.com>
2014-12-23 17:03:17 -0800

    Initial commit!

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', views.hello),
+    url(r'^$',views.home),
+    url(r'^time/$', views.current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
+)
