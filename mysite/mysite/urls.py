33dc036c3e46797b7f975fc93131684de7e4983e
GotThatHub <sperry@lrwonline.com>
2015-01-13 14:20:33 -0800

    1_13_15 push

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,7 +9,8 @@ urlpatterns = patterns('',
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', views.hello),
-    url(r'^$',views.home),
+    url(r'^$', views.home),
     url(r'^time/$', views.current_datetime),
     url(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
+    url(r'^antigravity/$', views.test_anti)
 )
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -20,3 +20,7 @@ def hours_ahead(request, offset):
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
     html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
     return HttpResponse(html)
+
+def test_anti(request):
+    import antigravity
+    return HttpResponse("<html><p>Did it work?</p></html>")
